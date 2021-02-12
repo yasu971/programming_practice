f66cadeed3375018a4a099a1795d20a811df8013
yasu971 <yasuishii1421@gmail.com>
2021-02-12 22:03:39 +0900

    Update 20210212104128_create_questions.rb
    create_questions
    マイグレーションファイルを編集。

--- db/migrate/20210212104128_create_questions.rb
+++ db/migrate/20210212104128_create_questions.rb
@@ -1,7 +1,12 @@
 class CreateQuestions < ActiveRecord::Migration[6.0]
   def change
     create_table :questions do |t|
-
+      t.string :title
+      t.string :content
+      t.string :option1
+      t.string :option2
+      t.string :option3
+      t.string :option4
       t.timestamps
     end
   end
