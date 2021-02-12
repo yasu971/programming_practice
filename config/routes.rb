c71c30f7809fe8265f2dfd814ace52f1b0935e78
yasu971 <yasuishii1421@gmail.com>
2021-02-12 10:41:56 +0900

    Update routes.rb
    6行目に以下を追加。
    resources :questions, only: [:new]

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'questions/index'
   root to: "questions#index"
+
+  resources :questions, only: [:new]
+  # resources :prototypes, only: [:index, :new, :create, :show, :edit, :update, :destroy]
 end
