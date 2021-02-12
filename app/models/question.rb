09ffb297750559a830852bd0705c1893348195ca
yasu971 <yasuishii1421@gmail.com>
2021-02-12 23:01:17 +0900

    Update question.rb

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,10 @@
 class Question < ApplicationRecord
+  belongs_to :user
+
+  validates :title,       presence: true
+  validates :content,     presence: true
+  validates :option1,     presence: true
+  validates :option2,     presence: true
+  validates :option3,     presence: true
+  validates :option4,     presence: true
 end
