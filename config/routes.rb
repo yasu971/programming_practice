244f3367d2affe885536a499ee7f8fd5cdf0a5f1
yasu971 <yasuishii1421@gmail.com>
2021-02-16 19:50:50 +0900

    Update routes.rb
    ルーティング設定。
    問題投稿ページ。
    questions/new2

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'questions/index'
   get 'questions/answer'
+  get 'questions/new2'
   root to: "questions#index"
 
-  resources :questions, only: [:new]
+  resources :questions, only: [:index, :new, :create]
   # resources :prototypes, only: [:index, :new, :create, :show, :edit, :update, :destroy]
 end
