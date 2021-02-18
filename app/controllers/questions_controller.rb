ed3e3f1dc403203170daeacf6728ce0bdfc2c1e6
yasu971 <yasuishii1421@gmail.com>
2021-02-18 21:59:01 +0900

    Update questions_controller.rb

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -7,10 +7,6 @@ class QuestionsController < ApplicationController
     @questions = Question.all
   end
   
-  # def new
-  #   @questions = Question.all
-  # end
-
   def new
     @question = Question.new
   end
@@ -21,6 +17,29 @@ class QuestionsController < ApplicationController
 
   def show
     @question = Question.find(params[:id])
+  end
+
+  def edit
+    @question = Question.find(params[:id])
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
+
+  def update
+    @question = Question.find(params[:id])
+    if question.update(question_params)
+      redirect_to question_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    question = Question.find(params[:id])
+    if question.destroy
+      redirect_to root_path
+    end
   end
 
   def create
@@ -38,59 +57,3 @@ class QuestionsController < ApplicationController
   end
   
 end
-
-# 「protospace-31747/app/controllers/prototypes_controller.rb」をコピー
-# class PrototypesController < ApplicationController
-#   before_action :authenticate_user!, except: [:index, :show]
-
-#   def index
-#     @prototypes = Prototype.all
-#   end
-
-#   def new
-#     @prototype = Prototype.new
-#   end
-
-#   def show
-#     @prototype = Prototype.find(params[:id])
-#     @comment = Comment.new
-#     @comments = @prototype.comments.includes(:user)
-#   end
-
-#   def edit
-#     @prototype = Prototype.find(params[:id])
-#     unless user_signed_in?
-#       redirect_to action: :index
-#     end
-#     end
-
-#   def update
-#     prototype = Prototype.find(params[:id])
-#     if prototype.update(prototype_params)
-#       redirect_to prototype_path
-#     else
-#       render :edit
-#     end
-#   end
-
-#   def destroy
-#     prototype = Prototype.find(params[:id])
-#     if prototype.destroy
-#       redirect_to root_path
-#     end
-#   end
-
-#   def create
-#     @prototype = Prototype.create(prototype_params)
-#     if @prototype.save
-#       redirect_to root_path
-#     else
-#       render :new
-#     end
-#   end
-
-#   private
-#   def prototype_params
-#     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
-#   end
-# end
