25f3e01f7491284e3b849d779009564908dc34eb
yasu971 <yasuishii1421@gmail.com>
2021-02-21 14:32:09 +0900

    Update routes.rb
    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
 
   resources :questions, only: [:index, :new, :create, :show, :edit, :update, :destroy]
 
+  resources :questions do
+    resources :comments, only: :create
+  end
+  
   resources :users, only: :show
   
 end
